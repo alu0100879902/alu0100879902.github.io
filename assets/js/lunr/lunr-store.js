d3178769bd2f88f8d3409f5b44194e5be0d7c5a0
alu0100879902 <alu0100879902@ull.edu.es>
2021-12-29 16:57:52 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Aprendiendo a usar un Generador Estático",
+        "excerpt":"Por fin jueves Y hoy por lo menos el cañón ha funcionado, y el Barsa ganó ayer… A ver cómo hacemos la tarea de Guarro… No entiendo por qué no me deja aceptar las tareas, siempre meto la pata con la informática, no tengo remedio. YA ME DEJÓ ACEPTAR LAS...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/aet",
+        "teaser": null
+      },{
+        "title": "Un día tranquilo",
+        "excerpt":"Un día tranquilo Cada vez nos mandan más trabajos, pero esto ya sabíamos que era así… Ya tengo la tarea de Guarro casi terminada, al final nada es imposible jeje Me gusta mucho comer naranjas Me gusta mucho comer peras Me gusta mucho comer limones Me gusta mucho comer fresas...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Clase del jueves 11/11/2021   Añadiendo otra clase…   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase",
+        "teaser": null
+      },{
+        "title": "¿Cómo enseñar a los jóvenes a informarse?",
+        "excerpt":"El fenómeno de la desinformación El fenómeno de la desinformación nunca ha sido ajeno al mundo educativo, pero pareciera que todo se ha acelerado tras la pandemia de COVID-19. Los y las jóvenes están expuestos a una gran cantidad de información, ya sea a través de las narrativas creadas por...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/articulo1",
+        "teaser": null
+      },{
+        "title": "Educar en la verdad",
+        "excerpt":"Contra las Fake News Combatir la desinformación en un mundo digitalizado resulta complejo porque los mensajes engañosos, mediados por la tecnología, adoptan cada vez formas más sofisticadas que imitan la apariencia de la información más fiable. A pesar de ello, en el ámbito científico, podemos decir que se ha avanzado...","categories": ["educacion"],
+        "tags": [],
+        "url": "/educacion/articulo2",
+        "teaser": null
+      }]
